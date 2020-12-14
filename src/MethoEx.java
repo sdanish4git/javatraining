cc3e7386495ff25d87d0af447a2edf1ba8dac7b6
Shakil Danish <sdanish4git@users.noreply.github.com>
2020-12-14 13:03:21 +0530

    test

---
+++ src/MethoEx.java
@@ -0,0 +1,36 @@
+
+public class MethoEx {
+	//public private pretected ----- default
+	int result;  //instance variable
+	static int road=20; //static variabl
+	
+	
+	public void localVariabl() {
+		int i=9;
+		System.out.println(i);
+		//we must have to initailize local variaible before using
+		//System.out.println(result);
+	}
+	
+	
+	
+	
+	//accessspecifier/modifier returntype name(parameter){...}
+	
+	/*public void msgPrint() {
+		System.out.println("Hello World");
+	}
+	
+	public void doSum(int n1,int n2) {
+		int sum=n1+n2; //local variable
+		System.out.println("sum="+sum);
+		
+		//sum=9;
+	}
+	
+	public int doSumOfTwo(int n1,int n2) {
+		int sum=n1+n2;
+		return sum;
+	}*/
+
+}
---
+++ src/Test.java
@@ -0,0 +1,37 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+/*		obj.msgPrint();
+		obj.doSum(5, 8);
+		int result=obj.doSumOfTwo(9, 5);*/
+//		System.out.println(result);
+		
+		MethoEx ashif=new MethoEx();
+		ashif.result=50;
+		MethoEx.road=100;
+		System.out.println("ashif's result="+ashif.result);
+		System.out.println("ashif's result="+MethoEx.road);
+		
+		MethoEx nehal=new MethoEx();
+		
+		System.out.println("Nehal's result="+nehal.result);
+		System.out.println("Nehal's result="+MethoEx.road);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
